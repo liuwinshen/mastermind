ddbc82002b72e5add241a4094413c4e670831f28
Winshen <winshen@uchicago.edu>
2018-06-18 17:08:13 -0500

    successfully tested that code contains at least one of the possible colors

--- mastermind_spec.rb
+++ mastermind_spec.rb
@@ -2,19 +2,19 @@ require_relative './mastermind'
 
 RSpec.describe Mastermind do
   game = Mastermind.new
+  code = game.generate_code
+
   it "prints game instructions" do
     expect { game.print_instructions }.to output(/Welcome to Mastermind!/).to_stdout
   end
 
   it "generates a random code of length 4" do
-    code = game.generate_code
     expect(code.length).to eq (4)
   end
 
-  it "generates a random code of 4 colors" do
-    code = game.generate_code
-    code.each do |color|
-      expect(color).to be_in(["R", "O", "Y", "G", "B", "P"])
+  it "code contains at least one of the possible colors" do
+    code.each do |char|
+      expect(["R", "O", "Y", "G", "B", "P"].include?(char)).to eq(true)
     end
   end
 end
