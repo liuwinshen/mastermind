26bd9f505170d3639b42c065136027ffcf66fef2
Winshen <winshen@uchicago.edu>
2018-06-19 10:57:38 -0500

    separated classes for overall game flow, code, and user input

--- mastermind.rb
+++ mastermind.rb
@@ -7,34 +7,57 @@ class Mastermind
         "\nTo make a guess: type the first letter of each color you're guessing in caps. "\
         "For example, if you want to guess 'red, orange, yellow, red,' type 'ROYR'."\
 
-        "\nAfter each guess, you will receive feedback letting you know:"\
-          "\n\t- how many colors are correct but in the wrong position"\
-          "\n\t- how many colors are correct and in the right position."\
+        "\nAfter each guess, you will receive feedback in the form of colored pins, letting you know:"\
+          "\n\t- how many colors are correct but in the wrong position (white pins)"\
+          "\n\t- how many colors are correct and in the right position (red pins)"\
 
         "\nTo quit the game, type '/q'. To start a new game, type '/r'."
     end
 
+  def compare(code, guess)
+  end
+end
+
+class Code
+  attr_reader :code
+
   def generate_code
-    code = []
+    @code = []
     colors = ["R", "O", "Y", "G", "B", "P"]
     while code.length < 4 do
       random_index = Random.rand(0..5)
-      code << colors[random_index]
+      @code << colors[random_index]
     end
-    return code
+    return @code
   end
+end
+
+class Input
+  attr_reader :user_input
 
   def input_guess
     print "Input a guess (available colors: R, O, Y, G, B, P): "
-    guess = gets
-
-    begin
-      guess.upcase
-    rescue
-      raise "Please enter only letters."
-    end
+    @user_input = gets
   end
 
-  # def compare(code, guess)
-  #   code = code.join()
+  def validate_guess(guess = @user_input)
+    invalid_guess = true
+    while invalid_guess
+      begin
+        guess = guess.upcase.gsub(/[,/s]/, "")
+      rescue NoMethodError
+        puts "Please enter only available colors: R, O, Y, G, B, P."
+        input_guess
+      end
+
+      if guess == /[ROYGBP][ROYGBP][ROYGBP][ROYGBP]/
+        invalid_guess = false
+      else
+        puts "Please enter only available colors: R, O, Y, G, B, P."
+        input_guess
+      end
+    end
+    return guess
+  end
+
 end
--- mastermind_spec.rb
+++ mastermind_spec.rb
@@ -1,33 +1,29 @@
 require_relative './mastermind'
 
 RSpec.describe Mastermind do
-  game = Mastermind.new
-  code = game.generate_code
-
   it "prints game instructions" do
-    expect { game.print_instructions }.to output(/Welcome to Mastermind!/).to_stdout
+    game = Mastermind.new
+    expect {game.print_instructions}.to output(/Welcome to Mastermind!/).to_stdout
   end
 
   it "generates a random code of length 4" do
+    code = Code.new.generate_code
     expect(code.length).to eq (4)
   end
 
   it "generates a code using at least one of the possible colors" do
+    code = Code.new.generate_code
     code.each do |char|
       expect(["R", "O", "Y", "G", "B", "P"].include?(char)).to eq(true)
     end
   end
 
-  it "generates a different code each time" do
-    code2 = game.generate_code
-    expect(code.object_id != code2.object_id).to eq (true)
-  end
-
   it "takes in user input" do
-    expect(game.input_guess).not_to eq (nil)
+    expect(Input.new.input_guess).not_to eq (nil)
   end
 
-  # it "only allows letters in input" do
-  #   expect(game.input_guess)
-  # end
+  it "only allows letters in input" do
+    expect{Input.new.validate_guess(guess = 123)}.to output(/Please enter only available colors/).to_stdout
+    expect{Input.new.validate_guess(guess = "abc")}.to eq("ABC")
+  end
 end
