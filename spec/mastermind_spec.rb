29ba3a93dcd0ec75ae9aaf455a625cef4fe7c4b0
Winshen <winshen@uchicago.edu>
2018-06-25 09:31:19 -0500

    successfully compares guess to code and returns red/white pin count

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -5,42 +5,13 @@ RSpec.describe Mastermind do
 
   describe "#print_instructions" do
     it "prints how to play" do
-      expect {game.print_instructions}.to output(/After each guess/).to_stdout
+      expect {game.print_instructions(["red", "orange", "yellow", "green", "blue", "purple"])}.to output(/After each guess/).to_stdout
     end
   end
 
-  describe "#color_count" do
-    it "counts how many times color appears in a given code" do
-      expect(game.color_count(["R", "O", "Y", "G", "B", "P"])).to eq ({:R => 1, :O => 1, :Y => 1, :G => 1, :B => 1, :P => 1})
+  describe "#play" do
+    xit "returns game outcome" do
+      expect (game.play).to eq ()
     end
-
-    it "counts repeated colors in code" do
-      expect(game.color_count(["P", "P", "P", "P"])).to eq ({:P => 4})
-      expect(game.color_count(["R", "O", "B", "R", "B", "B"])).to eq ({:R => 2, :O => 1, :B => 3})
-    end
-  end
-
-  describe "#white_pins" do
-    context "when guess matches some colors in code"
-      it "counts number of correct colors regardless of position" do
-        code_color_count = game.color_count(["R", "O", "Y", "G"])
-        guess_color_count = game.color_count("RROO")
-        expect(game.white_pins(code_color_count, guess_color_count)).to eq (2)
-
-        code_color_count = game.color_count(["R", "R", "Y", "G"])
-        guess_color_count = game.color_count("RRRO")
-        expect(game.white_pins(code_color_count, guess_color_count)).to eq (2)
-
-        code_color_count = game.color_count(["R", "O", "O", "O"])
-        guess_color_count = game.color_count("RROO")
-        expect(game.white_pins(code_color_count, guess_color_count)).to eq (3)
-      end
-
-    context "when guess matches no colors in code"
-      it "returns zero" do
-        code_color_count = game.color_count(["R", "O", "Y", "G"])
-        guess_color_count = game.color_count("BPBP")
-        expect(game.white_pins(code_color_count, guess_color_count)).to eq (0)
-      end
   end
 end
