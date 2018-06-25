f44ee91a36c3698e227653bf7b49833c23956562
Winshen <winshen@uchicago.edu>
2018-06-25 11:37:31 -0500

    added Play method to loop through prompting user guesses until none remaining

--- lib/game_start.rb
+++ lib/game_start.rb
@@ -2,19 +2,11 @@ require_relative './mastermind'
 require_relative './code'
 require_relative './inputguess'
 
-game = Mastermind.new
 code = Code.new
 short_colors = code.short_colors
 long_colors = code.long_colors
 input = InputGuess.new
+game = Mastermind.new(code, input)
 
 game.print_instructions(long_colors)
-user_guess = input.input(short_colors)
-
-puts "\tFor the purposes of this PR, I'm going to tell you the secret code:
-        #{code.code}.
-
-      The number of times each color appears in the code is
-        #{code.color_count(code.code)}.
-      The number of times each color appears in your guess is
-        #{code.color_count(user_guess)}."
+game.play(short_colors)
